96ddbe6658cdc48f44194b22ba19abe551185ab7
CristianAd <sonlatin2@gmail.com>
2023-08-28 17:37:21 -0500

    archivo de inicio

---
+++ data/data.js
@@ -0,0 +1,31 @@
+export const data = [
+    {
+        question: 'Cual des estas opciones es un editor de texto',
+        choices: [
+            'google', 
+            'notepad',
+            'Firefox'
+        ],
+        answer: "notepad"
+    },
+    {
+        question: 'Qué lenguaje no es orientado a objetos',
+        choices: [
+            'java', 
+            'Haskell',
+            'C++', 
+            'Python'
+        ],
+        answer: "Haskell"
+    },
+    {
+        question: 'Qué lenguaje nos sirve para estilizar sitios web',
+        choices: [
+            'Kotlin', 
+            'Tailwin',
+            'C++', 
+            'Python'
+        ],
+        answer: "Tailwin"
+    }
+]
